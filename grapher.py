eb63eadede988bc97dd765b08c96a1b2c95cad4c
Prabesh Subedi <prabesh.sb@gmail.com>
2024-11-20 21:27:13 +0545

    Add loop to read and graph

--- grapher.py
+++ grapher.py
@@ -3,16 +3,28 @@ from plotter import plot_graph
 from infix_evaluator import evaulate_infix
 
 
+def prompt():
+    print("Enter an expression(Ctrl+C to exit).")
+    expression = input("f(x) = ")
+    expression.replace(" ", "").lower().replace("exp", "e")
+    return expression
+
+
 def main():
-    expression = input("f(x) = ").replace(" ", "").lower().replace("exp", "e")
-    inputs = np.linspace(-10, 10, 100)
-    outputs = [evaulate_infix(expression.replace(
-        "x", str(num))) for num in inputs]
+    try:
+        while True:
+            expression = prompt()
+            inputs = np.linspace(-10, 10, 100)
+            outputs = [evaulate_infix(expression.replace(
+                "x", str(num))) for num in inputs]
 
-    valid_inputs = [inputs[i] for i, y in enumerate(outputs) if y is not None]
-    valid_outputs = [y for y in outputs if y is not None]
+            valid_inputs = [inputs[i]
+                            for i, y in enumerate(outputs) if y is not None]
+            valid_outputs = [y for y in outputs if y is not None]
 
-    plot_graph(valid_inputs, valid_outputs)
+            plot_graph(valid_inputs, valid_outputs)
+    except KeyboardInterrupt:
+        print("\nProgram exited by user.")
 
 
 if __name__ == "__main__":
